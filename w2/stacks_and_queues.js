b4261e745921b6c8bc9fcc380cacf5ce210a957c
Neil M <neilm813@gmail.com>
2020-04-13 09:08:46 -0700

    algo requirements

---
+++ w2/stacks_and_queues.js
@@ -0,0 +1,14 @@
+/* 
+  - A stack is a LIFO (Last in First Out) data structure
+  - create a class to represent a stack using an array & another class for a stack using a singly linked list
+    - create methods for each: push, pop, isEmpty, size, peek (return top item without removing)
+*/
+
+class NameOfClass {
+  constructor(parameter1, parameter2) {
+    this.prop1 = parameter1;
+    this.prop2 = parameter2;
+  }
+
+  methodName(parameters) {}
+}
