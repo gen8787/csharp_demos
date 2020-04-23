80db175176e4fcd459495c7a2c7e88b40b56cb5f
Neil M <neilm813@gmail.com>
2020-04-23 08:53:59 -0700

    algo reqs

--- dList.js
+++ dList.js
@@ -3,6 +3,10 @@
     - create the necessary classes to design a double linked list
       - a linked list that allows you to traverse forwards AND backwards, allowing you to start at the front or the back
     
+  w3d4
+    - DList: Prepend Value
+      - add new val before a target val
+    - isNodeInLeftHalf: given a reference to a node, return whether it's in left half or not
 */
 
 class DLLNode {
