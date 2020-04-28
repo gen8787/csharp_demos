2ceecf937abab810cf3fc2e5078e17cfc95bc3e1
Neil M <neilm813@gmail.com>
2020-04-28 09:12:46 -0700

    bst

--- BST.js
+++ BST.js
@@ -3,6 +3,18 @@
     - create the BST class and the BSTNode class
       - a BST has a root instead of a head (because it's a tree)
       - a BSTNode has a left, right, and val or data instead of next
-    - BST Max with and without recursion
-    - BST Min with and without recursion
+    - BST Max return the max with and without recursion
+    - BST Min return the min with and without recursion
+*/
+
+/* 
+                  50
+                /   \
+              40    60
+            /  \    / \
+          30   45 55  70
+        /   \        /
+      20    35     65
+        \
+        25
 */
