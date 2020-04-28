685bd638198de4604a2ac435dabf4044ae7ae34b
Neil M <neilm813@gmail.com>
2020-04-28 08:52:52 -0700

    bst

---
+++ BST.js
@@ -0,0 +1,8 @@
+/* 
+  w3d2
+    - create the BST class and the BSTNode class
+      - a BST has a root instead of a head (because it's a tree)
+      - a BSTNode has a left, right, and val or data instead of next
+    - BST Max with and without recursion
+    - BST Min with and without recursion
+*/
