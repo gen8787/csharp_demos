8ddb980c39862fb60f3c72c49b0eb343dc87f7f7
Neil M <neilm813@gmail.com>
2020-06-01 09:25:52 -0700

    linked list intro

---
+++ week1/sList.js
@@ -0,0 +1,27 @@
+/* 
+1. Mon
+  - isEmpty
+  - insertAtBack
+  - seedFromArr
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SList {
+  constructor() {
+    this.head = null;
+  }
+
+  isEmpty() {
+    // your code here
+  }
+
+  insertAtBack() {
+    // your code here
+  }
+}
