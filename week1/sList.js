8c9445f3f5bb70b850150bdfbaa76487157cea19
Neil M <neilm813@gmail.com>
2020-06-01 10:29:15 -0700

    algos

--- week1/sList.js
+++ week1/sList.js
@@ -19,10 +19,53 @@ class SList {
   }
 
   isEmpty() {
-    // your code here
+    return this.head === null;
   }
 
-  insertAtBack() {
-    // your code here
+  insertAtBack(val) {
+    const newNode = new Node(val);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      return;
+    }
+
+    let runner = this.head;
+
+    while (runner.next !== null) {
+      runner = runner.next;
+    }
+    runner.next = newNode;
+  }
+
+  seedFromArr(arr) {
+    for (const elem of arr) {
+      this.insertAtBack(elem);
+    }
+  }
+
+  display() {
+    let str = "";
+
+    let runner = this.head;
+
+    while (runner !== null) {
+      str += runner.data;
+
+      // add ", " if not last node
+      if (runner.next !== null) {
+        str += ", ";
+      }
+      runner = runner.next;
+    }
+    console.log(str);
+    return str;
   }
 }
+
+const linkedList = new SList();
+// linkedList.insertAtBack(1);
+// linkedList.insertAtBack(2);
+// linkedList.insertAtBack(3);
+linkedList.seedFromArr([1, 2, 3, 5, 6, 7, 8, 9, 10]);
+linkedList.display();
