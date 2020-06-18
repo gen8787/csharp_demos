cd43afbe74acf28472ff51021bc25a94b726eb36
Neil M <neilm813@gmail.com>
2020-06-18 09:04:46 -0700

    algo reqs

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -4,10 +4,9 @@
 
   Week3
     4. Thur
-      - A Doubly Linked List is a singly linked list with the added functionality of being able to traverse in both directions.
-      - Since you can traverse forwards or backwards, that means you should be able to start at the head or tail (end). After creating the necessary classes, then build the following methods:
-
+      - Create the necessary classes then build the below methods:
         - insertAtBack
           - given some new data, add it to the back of the DList
+        - insertAtFront
         - removeMiddleNode
 */
