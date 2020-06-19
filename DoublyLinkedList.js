7cff9b23effadde1d6545261e65ca7766e578784
Neil M <neilm813@gmail.com>
2020-06-19 09:18:21 -0700

    algos

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -9,6 +9,12 @@
           - given some new data, add it to the back of the DList
         - insertAtFront
         - removeMiddleNode
+
+  5. Fri
+    - insertAfter
+      - add new val after a target val
+    - insertBefore
+      - add new val before a target val
 */
 
 class DLLNode {
@@ -59,5 +65,85 @@ class DoublyLinkedList {
       this.tail = newTail;
     }
     return this;
+  }
+
+  // Time: O(n / 2) -> O(n) linear, n = list length
+  // Space: O(1) constant
+  removeMiddleNode() {
+    let forwardRunner = this.head;
+    let backwardsRunner = this.tail;
+
+    while (forwardRunner && backwardsRunner) {
+      // move the runners first so if the head is only node
+      // it won't be compared to itself and considered the middle
+      forwardRunner = forwardRunner.next;
+      backwardsRunner = backwardsRunner.prev;
+
+      if (forwardRunner === backwardsRunner) {
+        let midNode = forwardRunner;
+
+        midNode.prev.next = midNode.next;
+        midNode.next.prev = midNode.prev;
+        return midNode.data;
+      }
+      if (forwardRunner.next === backwardsRunner) {
+        /**
+         * happens if even lengthed list, there is no middle
+         * so return to exit early before getting to the ends of list
+         * this will return undefined which is what .pop() returns if
+         * called on an empty array
+         */
+        return null;
+      }
+    }
+  }
+
+  // can also send a runner from both sides to find the val in fewer iterations
+  // Time: O(n) linear, n = list length. targetVal could be at opposite of starting side
+  // Space: O(1)
+  insertAfter(targetVal, newVal) {
+    const newNode = new DLLNode(newVal);
+    let runner = this.head;
+
+    while (runner) {
+      if (runner.data === targetVal) {
+        newNode.prev = runner;
+        newNode.next = runner.next;
+        runner.next.prev = newNode;
+        runner.next = newNode;
+
+        if (runner === this.tail) {
+          this.tail = newNode;
+        }
+        return true;
+      } else {
+        runner = runner.next;
+      }
+    }
+    return false;
+  }
+
+  // Time: O(n) linear, n = list length. targetVal could be at opposite of starting side
+  // Space: O(1)
+  insertBefore(targetVal, newVal) {
+    const newNode = new DLLNode(newVal);
+    let runner = this.head;
+
+    while (runner) {
+      if (runner.data === targetVal) {
+        newNode.next = runner;
+        newNode.prev = runner.prev;
+        runner.prev.next = newNode;
+        runner.prev = newNode;
+
+        if (runner === this.head) {
+          this.head = newNode;
+        }
+        return true;
+      } else {
+        runner = runner.next;
+      }
+    }
+    return false;
   }
 }
