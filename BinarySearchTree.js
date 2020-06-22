61b912c97eedd5e626bb1391dea8dc4f3ded227d
Neil M <neilm813@gmail.com>
2020-06-22 09:05:37 -0700

    bst algo reqs

---
+++ BinarySearchTree.js
@@ -0,0 +1,30 @@
+/* 
+  1. Mon
+    1. isEmpty
+    2. min (with & without recursion)
+    3. max (with & without recursion)
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {}
+
+  min() {}
+
+  minRecursive() {}
+
+  max() {}
+
+  maxRecursive() {}
+}
