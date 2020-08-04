5d9c4d8f41cdcea23c9fead23263bf32ab3fc34d
Neil M <neilm813@gmail.com>
2020-08-04 10:19:30 -0700

    init proj

---
+++ week1/d2/GroceryStoreOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroceryStoreOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
