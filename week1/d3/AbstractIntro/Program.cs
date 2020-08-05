1bd51daeb1b71019b081086f4707fdcb55f21496
Neil M <neilm813@gmail.com>
2020-08-05 11:00:26 -0700

    proj init

---
+++ week1/d3/AbstractIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
