c1ea67346a1968e817faf042e8586a1605053fb2
Neil M <neilm813@gmail.com>
2020-08-11 21:06:42 -0700

    update & delete

--- week2/ForumDemo/Controllers/PostsController.cs
+++ week2/ForumDemo/Controllers/PostsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ForumDemo.Models;
@@ -46,6 +47,79 @@ namespace ForumDemo.Controllers
             // db does NOT update until you run this, after SaveChanges, the newPost now has it's ID from the DB
             db.SaveChanges();
             return RedirectToAction("All");
+        }
+
+        // {postId} is a URL parameter, it MUST match the asp-route-paramName attribute on the anchor/form tag
+        // where paramName is whatever you named it in the html
+        [HttpGet("/posts/{postId}")]
+        public IActionResult Details(int postId)
+        {
+            Post selectedPost = db.Posts.FirstOrDefault(post => post.PostId == postId);
+
+            if (selectedPost == null)
+            {
+                return RedirectToAction("All");
+            }
+
+            return View("Details", selectedPost);
+        }
+
+        [HttpPost("/posts/{postId}/delete")]
+        public IActionResult Delete(int postId)
+        {
+            Post selectedPost = db.Posts.FirstOrDefault(post => post.PostId == postId);
+
+            if (selectedPost != null)
+            {
+                db.Posts.Remove(selectedPost);
+                db.SaveChanges();
+            }
+            return RedirectToAction("All");
+        }
+
+        [HttpGet("/posts/{postId}/edit")]
+        public IActionResult Edit(int postId)
+        {
+            Post selectedPost = db.Posts.FirstOrDefault(post => post.PostId == postId);
+
+            if (selectedPost == null)
+            {
+                return RedirectToAction("All");
+            }
+            return View("Edit", selectedPost);
+        }
+
+        [HttpPost("/posts/{postId}/update")]
+        public IActionResult Update(Post editedPost, int postId)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // the id assignment line is happening automatically because our id param name matches the primary key name
+                // if not named the same need to do it manually or use a hidden input with the id in it b/c editedPost is a newly constructed object from the form data
+                // editedPost.PostId = idParamName;
+
+                // send back to the page with the form so error messages are displayed
+                return View("Edit", editedPost);
+            }
+
+            // ModelState IS valid
+            Post selectedPost = db.Posts.FirstOrDefault(post => post.PostId == postId);
+
+            if (selectedPost == null)
+            {
+                return RedirectToAction("All");
+            }
+
+            // selectedPost is not null
+            selectedPost.Topic = editedPost.Topic;
+            selectedPost.Body = editedPost.Body;
+            selectedPost.ImgUrl = editedPost.ImgUrl;
+            selectedPost.UpdatedAt = DateTime.Now;
+
+            db.Posts.Update(selectedPost);
+            db.SaveChanges();
+
+            return RedirectToAction("Details", new { postId = postId });
         }
     }
 }
